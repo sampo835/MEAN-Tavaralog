9ea5e6b8002f79df06ac038f6526ef92a87b9ebb
sampo835 <sampoheebo@gmail.com>
2024-02-06 14:31:15 +0200

    started making backend

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const config = require("./config/database");
+
+const app = express();
+
+// Connect To Database
+mongoose.connect(config.database);
+
+// On Connection
+mongoose.connection.on("connected", () => {
+  console.log("Connected to database " + config.database);
+});
+
+// On Error
+mongoose.connection.on("error", (err) => {
+  console.log("Database error " + err);
+});
+
+// Middleware
+app.use(bodyParser.json());
+
+// Import routes
+const itemsRoutes = require("./routes/items");
+const usersRoutes = require("./routes/users");
+
+// Use routes
+app.use("/items", itemsRoutes);
+app.use("/users", usersRoutes);
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+  database: "mongodb://127.0.0.1:27017/Inventory", // MongoDB connection string
+  //secret: "yoursecret", // Secret key for JWT (JSON Web Tokens) authentication
+};
