120716e688fcdb4033893b3b8adcaa92fe29841b
sampo835 <sampoheebo@gmail.com>
2024-02-14 14:06:59 +0200

    arduino stuff

--- angular-app/src/app/components/scan-admin/scan-admin.component.ts
+++ angular-app/src/app/components/scan-admin/scan-admin.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-scan-admin',
   templateUrl: './scan-admin.component.html',
-  styleUrl: './scan-admin.component.scss'
+  styleUrl: './scan-admin.component.scss',
 })
-export class ScanAdminComponent {
-
-}
+export class ScanAdminComponent {}
